87e19079426da767e0890cc1945159f092e9b012
Filip Wojciak <filip.wojciak@uni-oldenburg.de>
2023-10-17 01:50:57 +0200

    major update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <v-app>
-    <v-main>
-      <!-- Communication between child and parent components can be done using props and events. Props are attributes passed from a parent to a child and can be used within it.
-      A child component can emit events, which the parent then may react to. Here "selectedImage" is a prop passed to HomePage. HomePage emits the "fetchImgs" event,
-      which triggers the fetchImgs method in App.vue. In this demo this is technically not needed, but since it's a core element of Vue I decided to include it.-->
-      <HomePage
-        :selectedImage="selectedImage"
-        :currGallery="currGallery"
-        @loadImages="loadImages"
-        @updateSelected="updateSelected"
-        @getBlur="getBlur"
-        @resetGalery="resetGallery"
-      />
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <!-- Communication between child and parent components can be done using props and events. Props are attributes passed from a parent to a child and can be used within it.
+            A child component can emit events, which the parent then may react to. Here "selectedImage" is a prop passed to HomePage. HomePage emits the "fetchImgs" event,
+            which triggers the fetchImgs method in App.vue. In this demo this is technically not needed, but since it's a core element of Vue I decided to include it.-->
+            <HomePage :selectedImage="selectedImage" :currentGallery="currentGallery" @loadImages="loadImages" @updateSelected="updateSelected" @getBlur="getBlur" @resetGallery="resetGallery" />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
@@ -21,115 +14,115 @@ import HomePage from "./components/HomePage";
 import placeholder from "./assets/placeholder.jpg";
 
 export default {
-  name: "App",
+    name: "App",
 
-  components: {
-    HomePage,
-  },
-
-  data() {
-    return {
-      selectedImage: { url: placeholder, id: "placeholder" },
-      currGallery: [],
-    };
-  },
-
-  methods: {
-    /*
-    Fetch the first 80 images of a CEWE myPhotos user with a given cldId in a 300 pixel resolution.
-    Fetched images are saved within currGallery as data objects, which include the image ID, name, average color, timestamp, data type and the image url.
-
-    @param cldId The client ID of a CEWE myPhotos user, whose photos should be fetched.
-    */
-    async loadImages(cldId) {
-      // First fetch the ids of all the images on a users account, we need these in order to acquire the actual images in a given resolution
-      this.allImgData = await fetch(
-        "https://tcmp.photoprintit.com/api/photos/all?orderDirection=asc&showHidden=false&showShared=false&includeMetadata=false",
-        { headers: { cldId: cldId, clientVersion: "0.0.0-uni_webapp_demo" } }
-      ).then((idResponse) => idResponse.json());
-
-      this.limit = 80;
-      this.loadedAmount = 0;
-      this.currGallery = [];
-
-      // Fetch the actual image urls and other image info using image IDs saved in allImgData
-      for (const photo of this.allImgData.photos) {
-        // Stop once the limit is reached
-        if (this.loadedAmount >= this.limit) {
-          break;
-        }
-        this.loadedAmount += 1;
-
-        const data = {};
-        data.id = photo.id;
-        data.name = photo.name;
-        data.avgColor = photo.avgHexColor;
-        data.timestamp = photo.timestamp;
-        data.type = photo.mimeType;
-
-        const imgResponse = await fetch(
-          "https://tcmp.photoprintit.com/api/photos/" +
-            photo.id +
-            ".jpg?size=300&errorImage=false&cldId=" +
-            cldId +
-            "&clientVersion=0.0.0-uni_webapp_demo"
-        );
-        data.url = imgResponse.url;
-        this.currGallery.push(data);
-      }
+    components: {
+        HomePage,
     },
 
-    /*
-    Update the selected image visible on the HomePage.
-
-    @param slectedId ID of the selected image.
-    */
-    async updateSelected(selectedId, cldId) {
-      // Fetch the url to the image selected by the user in it's original resolution
-      const imgResponse = await fetch(
-        "https://tcmp.photoprintit.com/api/photos/" +
-          selectedId +
-          ".org?size=original&errorImage=false&cldId=" +
-          cldId +
-          "&clientVersion=0.0.0-uni_webapp_demo"
-      );
-      const image = this.currGallery.filter((obj) => {
-        return obj.id === selectedId;
-      })[0];
-      this.selectedImage = {
-        url: imgResponse.url,
-        id: selectedId,
-        name: image.name,
-        avgColor: image.avgColor,
-      };
+    data() {
+        return {
+            selectedImage: {
+                url: placeholder,
+                id: "placeholder"
+            },
+            currentGallery: [],
+            allImgData: [],
+            limit: 80,
+            loadedAmount: 0
+        };
     },
 
-    /*
-    Display a blurred version of the image.
-    The image is processed by the backend and sent to the app.
+    methods: {
 
-    @param selectedId ID of the image to be blurred.
-    */
-    async getBlur(selectedId, cldId) {
-      let localUrl = "http://127.0.0.1:8000/get-blur";
-      let url = localUrl + "/" + cldId + "/" + selectedId;
+        /* 
+          This method fetches the first 80 images from a user's gallery. 
+          It first retrieves all image IDs, then it fetches specific image data. 
+        */
+        async loadImages(cldId) {
 
-      let blurImg = await fetch(url, {
-        method: "get",
-      })
-        .then((response) => response.blob())
-        .then((imageBlob) => {
-          return URL.createObjectURL(imageBlob);
-        });
-      console.log(blurImg);
-      this.selectedImage.url = blurImg;
+            const headers = {
+                cldId: cldId,
+                clientVersion: "0.0.1-medienVerDemo",
+            };
+
+            // Fetch all image IDs from the user's gallery
+            const response = await fetch("https://tcmp.photoprintit.com/api/photos/all?orderDirection=asc&showHidden=false&showShared=false&includeMetadata=false", {
+                headers: headers
+            });
+            this.allImgData = await response.json();
+
+            // Reset current gallery and loaded amount before fetching new images
+            this.currentGallery = [];
+            this.loadedAmount = 0;
+
+            // Fetch detailed image info for each image up to the limit
+            for (const photo of this.allImgData.photos) {
+                if (this.loadedAmount >= this.limit) break;
+                this.loadedAmount++;
+
+                // Construct URL for specific image info and fetch data
+                const url = `https://tcmp.photoprintit.com/api/photos/${photo.id}.jpg?size=300&errorImage=false&cldId=${cldId}&clientVersion=0.0.0-uni_webapp_demo`;
+                const imgResponse = await fetch(url);
+                const imgUrl = imgResponse.url;
+
+                // Push image data to current gallery
+                this.currentGallery.push({
+                    id: photo.id,
+                    name: photo.name,
+                    avgColor: photo.avgHexColor,
+                    timestamp: photo.timestamp,
+                    type: photo.mimeType,
+                    url: imgUrl
+                });
+            }
+        },
+
+        /* 
+          This method updates the currently selected image. 
+          It fetches the high-resolution URL of the selected image and updates the selectedImage property. 
+        */
+        async updateSelected(selectedId, cldId) {
+
+            // Construct URL for fetching the high-resolution image
+            const url = `https://tcmp.photoprintit.com/api/photos/${selectedId}.org?size=original&errorImage=false&cldId=${cldId}&clientVersion=0.0.0-uni_webapp_demo`;
+            const response = await fetch(url);
+
+            // Find the image data in the current gallery
+            const image = this.currentGallery.find((obj) => obj.id === selectedId);
+
+            // Update the selected image with high-resolution URL and other details
+            this.selectedImage = {
+                url: response.url,
+                id: selectedId,
+                name: image.name,
+                avgColor: image.avgColor,
+            };
+        },
+
+        /* This method retrieves a blurred version of the selected image from the backend. */
+        async getBlur(selectedId, cldId) {
+
+            const localUrl = `http://127.0.0.1:8000/get-blur/${cldId}/${selectedId}`;
+
+            // Fetch the blurred image
+            const response = await fetch(localUrl);
+            const imageBlob = await response.blob();
+            const blurImgUrl = URL.createObjectURL(imageBlob);
+
+            // Update the selected image with the URL of the blurred image
+            this.selectedImage.url = blurImgUrl;
+        },
+
+        /* This method resets the current gallery and selected image. */
+        resetGallery() {
+
+            this.selectedImage = {
+                url: placeholder,
+                id: "placeholder"
+            };
+            this.currentGallery = [];
+        },
     },
-
-    // Resets cached images.
-    resetGallery() {
-      this.selectedImage = { url: placeholder, id: "placeholder" };
-      this.currGallery = [];
-    },
-  },
 };
 </script>
